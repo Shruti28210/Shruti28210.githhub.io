02e276e40e19ddd7d85f9418740190306ffa09f7
shruti <waghmodeshru@gmail.com>
2022-03-25 11:33:33 +0530

    Portfolio

---
+++ src/components/about.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import myImage from "../img/myImage.png";
+import logo1 from "../img/profile.png";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: [
+        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80",img:"https://cdn.worldvectorlogo.com/logos/html-1.svg" },
+        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75",img:"https://static.cdnlogo.com/logos/c/18/css.svg" },
+        {
+          id: "JavaScript_skill",
+          content: "JavaScript",
+          porcentage: "90%",
+          value: "90",
+          img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaSkrYIN-38LoMu5f3UTJ0HFFGMu0zZ68jX-r9ZoSJ0h0ip3_hdmVONCe9sUHdwbCoP4c&usqp=CAU"
+        },
+        { id: "PHP_skill", content: "NodeJs", porcentage: "70%", value: "70",img:"https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png" },
+        {
+          id: "Python_skill",
+          content: "ExpressJs",
+          porcentage: "75%",
+          value: "75",
+          img:"https://www.pngfind.com/pngs/m/136-1363736_express-js-icon-png-transparent-png.png"
+        },
+        
+        {
+          id: "ReactJS_skill",
+          content: "ReactJS",
+          porcentage: "80%",
+          value: "80",
+          img:"https://www.pinclipart.com/picdir/middle/537-5374089_react-js-logo-clipart.png"
+        },
+        {
+          id: "VanillaJS_skill",
+          content: "MongoDB",
+          porcentage: "85%",
+          value: "85",
+          img:"https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_plain_wordmark_logo_icon_146423.png"
+        },
+       
+      ],
+      about_me: [
+        {
+          id: "first-p-about",
+          content:
+            `I am a keen web enthusiast who loves to build complex systems and aspires to be a full stack web developer in the coming years.I have been able to utilize my skills by working on projects and honing my punctuality and work discipline at the same time. A very curious and self-motivated person, I am looking forward to learning about new technology and working in an energetic organization.
+
+            I am a team player, a professional and self motivated individual adept at HTML, CSS, JavaScript, MERN Stack and DSA.
+            
+            My leisure interests: Apart from this I like playing guitar`,
+        },
+        
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="box-shadow-full">
+                <div className="row">
+                <div className="col-md-6">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <h5 className="title-left">About Me</h5>
+                        {/* <a className="navbar-brand js-scroll" href="#page-top">
+                            <img src={this.state.logo1} alt="logo1"style={{ maxWidth: "100px" }}/></a> */}
+
+
+                            <div>
+                              <img src={logo1} style={{ maxWidth: "150px" }}/>
+                            </div>
+                      </div>
+                      {this.state.about_me.map((content) => {
+                        return (
+                          <p className="lead" key={content.id}>
+                            {content.content}
+                          </p>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="row">
+                      <div
+                        className="col-sm-6 col-md-5"
+                        style={{ margin: "0 auto" }}
+                      >
+                        <div
+                          className="about-img"
+                          style={{ textAlign: "center" }}
+                        >
+                          <img
+                            className="img-fluid rounded b-shadow-a"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="skill-mf">
+                      {/* <p className="title-s">Skill</p> */}
+                      {this.state.skills.map((skill) => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <img src={skill.img} width={30} id="img"></img>
+                            <span>{skill.content}</span>{" "}
+                            <span className="pull-right">
+                              {skill.porcentage}
+                            </span>
+                            <div className="progress">
+                              <div
+                                className="progress-bar"
+                                role="progressbar"
+                                style={{ width: skill.porcentage }}
+                                aria-valuenow={skill.value}
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/portfolio.jsx
@@ -0,0 +1,265 @@
+import React from "react";
+
+//import stock
+// import stock from "../img/image1.jpg";
+// import stock1 from "../img/image1.jpg";
+// import stock2 from "../img/image1.jpg";
+// import stock3 from "../img/image1.jpg";
+// import stock4 from "../img/image1.jpg";
+// import stock5 from "../img/image1.jpg";
+
+
+import bowl from "../img/bowl/bowlh1.png";
+import bowl1 from "../img/bowl/bowlh2.png";
+import bowl2 from "../img/bowl/bowllogin.png";
+import bowl3 from "../img/bowl/bowlmenu.png";
+import bowl4 from "../img/bowl/bowlsearch.png";
+import bowl5 from "../img/bowl/bowlcart.png";
+
+
+import dot from "../img/dot&key/doth.png";
+import dot1 from "../img/dot&key/dotfil.png";
+import dot2 from "../img/dot&key/dotfil2.png";
+import dot3 from "../img/dot&key/dotlog.png";
+import dot4 from "../img/dot&key/dotsign.png";
+import dot5 from "../img/dot&key/dotfooter.png";
+
+
+import koovs from "../img/koovs/koovsh.png";
+import koovs1 from "../img/koovs/koovs log.png";
+import koovs2 from "../img/koovs/koovsnav.png";
+import koovs3 from "../img/koovs/koovsabout.png";
+import koovs4 from "../img/koovs/koovsreadmore.png";
+import koovs5 from "../img/koovs/koovs footer.png";
+
+
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <section id="work" className="portfolio-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Portfolio</h3>
+                <p className="subtitle-a">
+                  Here My some Projets.
+                </p>
+                <div className="line-mf"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={bowl} data-lightbox="gallery-vmarine">
+                  <div className="work-img">
+                    <img src={bowl} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Bowl Company</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5| CSS | JS | MongoDB | NodeJs
+                          </span>
+                          <p>
+                            Developed the Cart Page, Shipping Page,Payment Page.
+                          </p>
+                          <p>This is a clone of the website The-Bowl-Company where we implemented some additional features for better user experience</p>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <div>
+                  <a href="https://github.com/MadhanM282/the-bowl-company.git" target="blank"><img id ="githubrepo" width ={40} src="https://seeklogo.com/images/G/github-logo-5F384D0265-seeklogo.com.png"></img></a>
+                  <a href="https://the-bowl-company.vercel.app/" target="blank"><button id = "livebtn">Live</button></a>
+                  </div>
+                <a
+                  href={bowl1}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={bowl2}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={bowl3}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={bowl4}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={bowl5}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={dot} data-lightbox="gallery-aguadeluz">
+                  <div className="work-img">
+                    <img src={dot} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Dot and Key SkinCare</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 | CSS3 | JS(ES6)
+                          </span>{" "}
+                          <p>Build Payment page and Store Data to Backend</p>
+                          <p>This is a clone of Dot and Key Skincare website, which is a new age skincare brand with products such as Serum, Moisturizer and skincare products.</p>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={dot1}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <div>
+                  <a href="https://github.com/MohdTazammul/Dot-and-Key-Skincare-clone.git" target="blank"><img id ="githubrepo" width ={40} src="https://seeklogo.com/images/G/github-logo-5F384D0265-seeklogo.com.png"></img></a>
+                  <a href="https://mohdtazammul.github.io/Dot-and-Key-Skincare-clone/" target="blank"><button id = "livebtn">Live</button></a>
+                  </div>
+                <a
+                  href={dot2}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={dot3}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={dot4}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={dot5}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={koovs} data-lightbox="gallery-todo">
+                  <div className="work-img">
+                    <img src={koovs} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Koovs.com</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 | CSS 
+                          </span>
+                          <p>Developed Men's page,Blog page,KOOV's page</p>
+                          <p>A clone of Koovs.com website which offers online store for western fashion, catering to menswear, womenswear, footwear, accessories, jewellery and beauty products.</p>
+                          {/*} / <span className="w-date">18 Sep. 2018</span>*/}
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={koovs1}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <div>
+                  <a href ="https://github.com/dimple06varshney/koovs.git" target="blank"><img id ="githubrepo" width ={40} src="https://seeklogo.com/images/G/github-logo-5F384D0265-seeklogo.com.png"></img></a>
+                  <a href ="https://dimple06varshney.github.io/koovs/" target="blank"><button id = "livebtn">Live</button></a>
+                  </div>
+                <a
+                  href={koovs2}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={koovs3}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={koovs4}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={koovs5}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
